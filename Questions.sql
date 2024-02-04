3876ce8c971e63915049a5a52b516a458bf12289
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-02-04 03:48:49 -0500

    Create Questions.sql

---
+++ Questions.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Questions (number INTEGER PRIMARY KEY, question TEXT);
+INSERT INTO Questions VALUES (1, "Is this a level request for the list or is it a record submission?");
+INSERT INTO Questions VALUES (2, "If it is a level request, what's the name of the level, ID, and creator?");
+INSERT INTO Questions VALUES (3, "If this is a record submission, send the video link and the name of the player completing it.");
