ec150e2c522902a7ac63a4f0c01d80e06ac9bc15
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-02-04 03:09:21 -0500

    Create players.sql

---
+++ players.sql
@@ -0,0 +1,7 @@
+CREATE TABLE players(
+  id INTEGER PRIMARY KEY, name TEXT, placement VARCHAR);
+INSERT INTO players VALUES (1, "Mystical Man", 1);
+INSERT INTO players VALUES (2, "Cheesy", 3);
+INSERT INTO players VALUES (3, "Eliks", 2);
+INSERT INTO players VALUES (4, "Kriz", "NA");
+INSERT INTO players VALUES (5, "LLucky Gaming", 4);
