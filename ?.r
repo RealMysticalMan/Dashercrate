8ba961cc75ec0a24deb1cebaab43437a1538e1cd
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-02-04 03:38:21 -0500

    Update ?.r

--- ?.r
+++ ?.r
@@ -1,1 +1,6 @@
-l
+library(rstudioapi)
+plot(1:10, 1:10, type = "n")
+text(5, 5, "Dashercrate", cex = 3)
+library(ggplot2)
+ggplot(data.frame(x = 5, y = 5), aes(x, y)) +
+geom_text(label = "Large Text", size = 10)
