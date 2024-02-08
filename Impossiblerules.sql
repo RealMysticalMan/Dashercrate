c6da0d741a376dce5b18f59fc63fa31052a97413
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-02-08 10:34:22 -0500

    Create Impossiblerules.sql

---
+++ Impossiblerules.sql
@@ -0,0 +1,7 @@
+CREATE NEW TABLE rules (id INTEGER PRIMARY KEY, rule TEXT);
+INSERT INTO rules VALUES (1, "The main rules are the same as on Pointercrate and Dashercrate");
+INSERT INTO rules VALUES (2, "Noclip accuracy is allowed as long as the accuracy is 100%");
+INSERT INTO rules VALUES (3, "There is no FPS cap");
+INSERT INTO rules VALUES (4, "The difficulty of a two player level will be judged by how difficult it is to complete solo");
+INSERT INTO rules VALUES (5, "At the time of their creation, levels must be harder than the hardest legitametly verified rated level, which can be found on Pointercrate");
+INSERT INTO rules VALUES (6, "Levels must be decorated");
