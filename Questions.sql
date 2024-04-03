f84b35421903047e860a44ae0ef17a6424618ffa
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-04-03 14:51:00 -0400

    Update Questions.sql

--- Questions.sql
+++ Questions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Questions (number INTEGER PRIMARY KEY, question TEXT);
 INSERT INTO Questions VALUES (1, "Is this a level request for the list or is it a record submission?");
 INSERT INTO Questions VALUES (2, "If it is a level request, what's the name of the level, ID, and creator?");
-INSERT INTO Questions VALUES (3, "If this is a record submission, send the video link and the name of the player completing it.");
+INSERT INTO Questions VALUES (3, "If this is a record submission, send the video link or screenshot and the name of the player completing it.");
 INSERT INTO Questions VALUES (4, "Make sure this isn't hacked and please tell me if it is so that I don't add it. If you do not disclose this, you risk getting in serious trouble.");
 INSERT INTO Questions VALUES (5, "Congratulations, you finished all of the questions on the form. You may close this tab now.");
