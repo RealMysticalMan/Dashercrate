b662646cf7ba9f9ba303ed62c759e9c1e0ccdc12
RealMysticalMan <150212097+RealMysticalMan@users.noreply.github.com>
2024-04-06 23:09:13 -0400

    Delete ?.r

--- ?.r
+++
@@ -1,6 +0,0 @@
-library(rstudioapi)
-plot(1:10, 1:10, type = "n")
-text(5, 5, "Dashercrate", cex = 3)
-library(ggplot2)
-ggplot(data.frame(x = 5, y = 5), aes(x, y)) +
-geom_text(label = "Large Text", size = 10)
